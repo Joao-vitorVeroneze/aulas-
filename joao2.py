386021193f82b3bc74a961e7ef15bd0b3671dad7
JOÃO VITOR VERONEZE DA FONSECA <joao-vitor_fonseca@estudante.sesisenai.org.br>
2024-10-03 19:52:23 -0300

    trabalhos de aula

---
+++ joao2.py
@@ -0,0 +1,150 @@
+#print ("hoje choveu")
+
+"""comentario de multiplas 
+linhas usando strigs"""
+
+'''comentario de multiplas 
+linhas usando strigs, pode ser simples ou duplas'''
+
+##variavel do tipo inteiro
+#idade = 25
+
+#variavel do tipo string
+#nome = "romario"
+
+# variavel do tipo float
+#altura = 1.75
+
+#variavel dp tipo bool
+#estudante = True
+
+#print(f"meu nome é",{nome}, "idade", {idade}, "e altura", {altura})
+
+#conta quantas letras tem no mensagem
+#mensagem = "João vitor veroneze"
+#print(len(mensagem))
+
+
+#mostra qual tipo de variavel
+#x=10
+#print(type(x)) 
+
+#y=3.14
+#print(type(y))
+
+#print(type(nome))
+
+#professor=False
+#print(type(professor))
+
+
+#pede o nome e depois fala o nome dizendo ola
+#nome = input("qual é seu nome? ")
+#print( "ola, "+ nome + "!")
+
+'''conversao de numeros '''
+
+#numero_float = 42.9
+#numero_inteiro = int(numero_float)
+#print(f"conversao de (numero_float) para inteiro: {numero_inteiro}")
+
+#numero_string = "123"
+#numero_inteiro = int (numero_string)
+#print (f"conversao de (numero_string) para numero {numero_inteiro}")
+#print(f"o tipo de variavel numero_inteiro E {type(numero_inteiro)}")
+
+#numero_string_float = "123.456"
+#numero_float = float(numero_string_float)
+#print (f"conversao de {numero_string_float} para numero {numero_float}")
+#print(f"o tipo de variavel numero_inteiro E {type(numero_float)}")
+#print(numero_float + 1)
+
+#numero_inteiro = 456
+#numero_string = str(numero_inteiro)
+#print(f"conversao de {numero_inteiro} para string {numero_string}")
+#print(f"o tipo de variavel numero_string e {type(numero_string)}")
+
+#numero_float = 456.789
+#numero_string_float = str(numero_float)
+#print(f"conversao de {numero_float} para string {numero_string_float}")
+#print(f"o tipo de variavel numero_string_float e {type(numero_string_float)}")
+
+#numero = int (input("digite um numero "))
+#print (f"o numero vale {numero} e o seu tipo é {type(numero)}")
+#print(f"a soma de",numero, "+ 1  " )
+#print(f"é { numero + 1 }")
+
+'''divisao de ponto fracionario'''
+
+#X = 22
+#y = 7
+#resultado_divisao_inteira = X // y
+#print (f"divisao inteira {X} // {y} = {resultado_divisao_inteira}")
+
+'''resto'''
+
+#X = 22
+#y = 7
+#resultado_divisao_inteira = X % y
+#print (f"o resto de  {X} e {y} = {resultado_divisao_inteira}")
+
+"""potencia"""
+
+#X = 2
+#y = 8
+#resultado_divisao_inteira = X ** y
+#print (f"o resto de  {X} ** {y} = {resultado_divisao_inteira}")
+
+'''exercicios'''
+
+'''att1'''
+
+#nro = float(input("digite um numero: "))
+#nro2 = float(input("digite um segundo numero: "))
+#soma = nro + nro2
+#print("a soma entre ",nro, "e", nro2, "é", soma)
+
+'''att2'''
+
+#nro = input("digite um numero: ")
+#numero_inteiro = int(nro)
+#dobro = numero_inteiro * 2
+#print(f"o dobro de ",numero_inteiro, "é", dobro)
+
+'''att3'''
+
+#nro = float(input("digite um numero: "))
+#nro2 = float(input("digite um segundo numero: "))
+#nroINT= int(nro)
+#nroINT2= int(nro2)
+#mult = nroINT* nroINT2
+#print("a multiplicação entre ",nroINT, "e", nroINT2, "é", mult)
+
+'''att4'''
+
+#altura = int(input("digite a altura do retangulo"))
+#base = int(input("digite a base do retangulo"))
+#area = altura * base
+#print (F"a area do retangulo é {area}")
+
+'''att5'''
+
+#celsius = float(input("digite uma temperatura em Celsius "))
+#f=((9/5)*celsius + 32)
+#print(F"a temperatura {celsius}em graus celsisus é {f} em Fahrenheit")
+
+
+'''atividades aula 03/10/24'''
+
+''' aspas'''
+
+#mesagem1 = "ola mundo"
+
+#mesagem2 = 'ola mundo'
+
+#mensagem3 = "ola 'mundo'"
+
+#mensagem4 = 'ola "mundo"'
+
+#print (mensagem)
+
---
+++ main.py
@@ -0,0 +1,6 @@
+nro = float(input("digite um numero: "))
+nro2 = float(input("digite um segundo numero: "))
+
+soma = nro + nro2
+
+print("a soma entre ",nro, "e", nro2, "é", soma)
